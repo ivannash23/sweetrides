669598c8ff28ec4744936d9f6c35be0248ffe83b
ivannash23 <ivannash23@Gmail.com>
2018-02-08 01:36:48 -0800

    translating mockups to front-end, ran into bootstrap bugs(old version FIXED), translating html to erb rails helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :sellers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'landing#index'
+
+
 end
