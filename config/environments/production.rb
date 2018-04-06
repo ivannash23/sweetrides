d16ae0d6e01cf025287e1736de7c05d83458c92e
ivannash23 <ivannash23@Gmail.com>
2018-04-05 21:50:54 -0700

    gmail env config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,18 +32,19 @@ Rails.application.configure do
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
-  config.action_mailer.default_url_options = { host: 'sweetrides.herokuapp.com' }
-
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'sweetrides.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
-   :authentication       => "plain",
-  :enable_starttls_auto => true
-}
+    user_name:      'sweetride831@gmail.com',
+    password:       '1qaz!QAZ2wsx@WSX',
+    domain:         'gmail.com',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
