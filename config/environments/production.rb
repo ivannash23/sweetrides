a98ad3aa4f79969382eb2e3cc580ae8d3e547581
ivannash23 <ivannash23@Gmail.com>
2018-04-09 21:52:04 -0700

    troubleshooting database for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,14 +37,14 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'sweetrides.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name:      'sweetride831@gmail.com',
-    password:       '2wsx@WSX2wsx@WSX',
-    domain:         'gmail.com',
-    address:       'smtp.gmail.com',
-    port:          '587',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  :address              =>  'smtp.sendgrid.net',
+  :port                 =>  '587',
+  :authentication       =>  :plain,
+  :user_name            =>  'ivannash23@heroku.com',
+  :password             =>  'flamengo1',
+  :domain               =>  'sweetrides.herokuapp.com',
+  :enable_starttls_auto  =>  true
+}
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
