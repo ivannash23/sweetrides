c60a286ff960761aaf9fb364189ae7cbbcc8f341
ivannash23 <ivannash23@Gmail.com>
2018-04-09 21:28:17 -0700

    troubleshooting database for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:      'sweetride831@gmail.com',
-    password:       '1qaz!QAZ2wsx@WSX',
+    password:       '2wsx@WSX2wsx@WSX',
     domain:         'gmail.com',
     address:       'smtp.gmail.com',
     port:          '587',
